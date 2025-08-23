6b081e3340d99a8b58cd9cd0764e995d8b4d8622
BigBabyofTel <augustus.tb@outlook.com>
2025-08-23 02:49:37 -0500

    mobile outline finished

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
+<script context="module">
+	export const ssr = false;
+</script>
+
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-
 	let { children } = $props();
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+
+	const age = 32
+
+	const ageBrokenDown = $derived({
+		years: 0,
+		month: 0,
+		day: 0,
+	})
+
+
+
+
+</script>
+
+
+
+
+
+<div class="bg-blue-500 p-2 w-full h-dvh flex flex-col items-center justify-center">
+	<h1 class="p-2 mb-24 text-5xl font-bold">Age Calculator</h1>
+	<div class="w-3/4 h-1/2 bg-yellow-600 flex flex-col items-center justify-center">
+
+		<div class="bg-green-400 w-3/4 h-1/2 p-5 flex flex-col items-center justify-center">
+			<p>This is my app now!!!!</p>
+			<span>
+			this will be a form
+		</span>
+		</div>
+		<div class="bg-purple-400 w-3/4 h-1/2 p-5 flex flex-col items-center justify-center">
+			<span>Age : {age}</span>
+			<span>{ageBrokenDown.years} Years</span>
+			<span>{ageBrokenDown.month} Months</span>
+			<span>{ageBrokenDown.day} Days</span>
+		</div>
+	</div>
+
+</div>
