b866c9a3e8ba7ea86a664186752b8201055374c9
BigBabyofTel <augustus.tb@outlook.com>
2025-08-26 03:29:20 -0500

    date input added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,49 @@
 <script lang="ts">
 
-	const age = 32
+
+	let age = $state('');
+
+	const birthday = $state({
+		month: '',
+		day: '',
+		year: '',
+	})
 
 	const ageBrokenDown = $derived({
 		years: 0,
 		month: 0,
-		day: 0,
-	})
+		day: 0
+	});
 
+	console.log(age)
 
+	function calculateAge(age: string)  {
+		let year: string, month: string, day: string;
+		year = age.substring(0, 4);
+		month = age.substring(5, 7);
+		day = age.substring(8, 10);
+		return { year, month, day };
+	}
 
-
+	const newAge = calculateAge(age);
 </script>
-
-
-
 
 
 <div class="bg-blue-500 p-2 w-full h-dvh flex flex-col items-center justify-center">
 	<h1 class="p-2 mb-24 text-5xl font-bold">Age Calculator</h1>
-	<div class="w-3/4 h-1/2 bg-yellow-600 flex flex-col items-center justify-center">
-
-		<div class="bg-green-400 w-3/4 h-1/2 p-5 flex flex-col items-center justify-center">
+	<div class="w-11/12 h-1/2 bg-yellow-600 flex flex-col items-center justify-center">
+		<div class="bg-green-400 w-full h-1/2 p-5 flex flex-col items-center justify-center">
 			<p>This is my app now!!!!</p>
-			<span>
-			this will be a form
-		</span>
+			<div class="w-full flex justify-around">
+				<span>Birthday : <input type="date" name="date" bind:value={age} />
+				</span>
+			</div>
 		</div>
 		<div class="bg-purple-400 w-3/4 h-1/2 p-5 flex flex-col items-center justify-center">
-			<span>Age : {age}</span>
-			<span>{ageBrokenDown.years} Years</span>
-			<span>{ageBrokenDown.month} Months</span>
-			<span>{ageBrokenDown.day} Days</span>
+			<span>Age : {newAge}</span>
+			<span>{newAge.year} Years</span>
+			<span>{newAge.month} Months</span>
+			<span>{newAge.day} Days</span>
 		</div>
 	</div>
 
